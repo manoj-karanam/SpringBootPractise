4e89070ad6635300fd45f40ab0926f8c1121cb39
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-11-30 15:46:47 -0500

    Strting with springboot added a hello and topics page

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -0,0 +1,24 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@RequestMapping("/topics")
+	public List<Topic> GetAllTopics() {
+		return Arrays.asList(
+				 
+				new Topic("Spring", "Spring Framework", "Spring Description"),
+				new Topic("Java", "Core Java", "Core Java Description"),
+				new Topic("JavaScript", "JavaScript", "JavaScript Description")
+				
+				);
+		
+	}
+	
+}
